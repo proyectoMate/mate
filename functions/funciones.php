6425011756d2b1768e11eebfc45b4177be0526c0
Erik <info.proyectomate@gmail.com>
2016-05-25 11:40:46 -0300

    Subir al master

---
+++ functions/funciones.php
@@ -0,0 +1,27 @@
+<?php
+	function head($titulo)
+	{
+?>
+	    <meta  Content-Type="text/html"charset="encoding">
+	    <meta  http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    	<title><?php $titulo ?></title>
+
+    	<!-- Core CSS - Include with every page -->
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+    	<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    	<!-- Page-Level Plugin CSS - Dashboard -->
+    	<link href="css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
+    	<link href="css/plugins/timeline/timeline.css" rel="stylesheet">
+
+    	<!-- SB Admin CSS - Include with every page -->
+    	<link href="css/sb-admin.css" rel="stylesheet">
+
+    	<!-- Page-Level Plugin CSS - Tables -->
+    	<link href="css/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+<?php
+	}
+
+?>
