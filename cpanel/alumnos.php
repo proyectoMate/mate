e3ab2c335eccc90300a21dbfee6a6889049b25f8
Erik Sanchez <jenriquesanchez@hotmail.com.ar>
2016-06-05 15:41:56 -0300

    crearción archivo alumnos.php

---
+++ cpanel/alumnos.php
@@ -0,0 +1,61 @@
+<?php
+    require("functions/generales.php");
+?>
+<!DOCTYPE html>
+<html>
+<?php
+head();
+
+?>
+<body>
+
+     <div id="wrapper">
+
+            <?php
+                top_menu();
+            ?>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3 class="page-header">Panel de Control / Bienvenido </h3>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-8">
+                    
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-8 -->
+                <div class="col-lg-4">
+                    <!-- /.panel .chat-panel -->
+                </div>
+                <!-- /.col-lg-4 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Core Scripts - Include with every page -->
+    <script src="../js/jquery-1.10.2.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/plugins/metisMenu/jquery.metisMenu.js"></script>
+
+    <!-- Page-Level Plugin Scripts - Dashboard -->
+    <script src="../js/plugins/morris/raphael-2.1.0.min.js"></script>
+    <script src="../js/plugins/morris/morris.js"></script>
+
+    <!-- SB Admin Scripts - Include with every page -->
+    <script src="../js/sb-admin.js"></script>
+
+    <!-- Page-Level Demo Scripts - Dashboard - Use for reference -->
+    <script src="../js/demo/dashboard-demo.js"></script>
+
+</body>
+
+</html>
