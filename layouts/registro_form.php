dc27c940c06e41ae182b9d26ab2cf0e7069afe77
Luis Vega <vegaluis.v@gmail.com>
2016-06-07 00:03:27 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  En la rama master  Su rama está delante de «origin/master« para 2 commits.    (use "git push" to publish your local commits)
     Cambios para hacer commit:
    	deleted:    encuentro_inst.sql
    	deleted:    encuentro_inst.sql.zip
    	deleted:    formulario.rar
    	deleted:    formulario/css.css
    	deleted:    formulario/formulario.html
    	deleted:    formulario/formulario.php
    	modificado: layouts/registro_form.php

--- layouts/registro_form.php
+++ layouts/registro_form.php
@@ -11,7 +11,7 @@ if (isset($_POST['button']))
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="nombre">Nombre</label>  
-  <div class="col-md-4">
+  <div class="col-md-5">
   <input id="nombre" name="nombre" type="text" placeholder="Jose" class="form-control input-md" required="">
     
   </div>
@@ -20,7 +20,7 @@ if (isset($_POST['button']))
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="apellido">Apellido</label>  
-  <div class="col-md-4">
+  <div class="col-md-5">
   <input id="apellido" name="apellido" type="text" placeholder="Perez" class="form-control input-md" required="">
     
   </div>
@@ -29,25 +29,23 @@ if (isset($_POST['button']))
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="dni">Dni</label>  
-  <div class="col-md-4">
+  <div class="col-md-5">
       <input id="dni" name="dni" type="number" placeholder="11223345" class="form-control input-md" required="">
-    
   </div>
 </div>
 
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="password">Password</label>  
-  <div class="col-md-4">
+  <div class="col-md-5">
       <input id="password" name="password" type="password" class="form-control input-md" required="">
-    
   </div>
 </div>
 
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="mail">Mail</label>  
-  <div class="col-md-4">
+  <div class="col-md-5">
       <input id="mail" name="email" type="email" placeholder="mail@dominio.com" class="form-control input-md" required="">
     
   </div>
@@ -56,8 +54,24 @@ if (isset($_POST['button']))
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="celular">Celular</label>  
-  <div class="col-md-4">
+  <div class="col-md-5">
       <input id="celular" name="tel" type="number" placeholder="1166448866" class="form-control input-md">
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="instituto">Instituto</label>  
+  <div class="col-md-5">
+      <input id="celular" name="escuela" type="text" class="form-control input-md" maxlength="150">
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="actividad">Cargo en la institucion</label>  
+  <div class="col-md-5">
+      <input id="celular" name="actividad" type="text" placeholder="Alumno, Directivo, Preceptor" class="form-control input-md">
   </div>
 </div>
 <input type="hidden" name="estado" value="0">
@@ -66,7 +80,7 @@ if (isset($_POST['button']))
 <!-- Button -->
 <div class="form-group">
   <label class="col-md-4 control-label" for="button"></label>
-  <div class="col-md-4">
+  <div class="col-md-5">
     <button id="button" name="button" class="btn btn-success">Registrarse</button>
   </div>
 </div>
